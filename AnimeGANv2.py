72752effec2cbc6d64722894821a18c4d2ffcca0
roger <1468667058@qq.com>
2022-06-06 10:47:51 +0800

    refactor: replace slef.config to wandb

--- AnimeGANv2.py
+++ AnimeGANv2.py
@@ -14,38 +14,13 @@ from tools.utils import *
 class AnimeGANv2(object):
     def __init__(self, args):
         self.hyperparameters = args.hyperparameters
-        config_defaults = {
-            'real_loss_weight': 1.7,
-            'fake_loss_weight': 1.7,
-            'gray_loss_weight': 1.7,
-            'real_blur_loss_weight': 1.0
-        }
-        self.model_name = 'AnimeGANv2'
-        self.checkpoint_dir = args.checkpoint_dir
-        self.log_dir = args.log_dir
-        self.dataset_name = args.dataset
-
-        config_defaults['epoch'] = args.epoch
-        config_defaults['init_epoch'] = args.init_epoch  # args.epoch // 20
-
-        config_defaults['gan_type'] = args.gan_type
-        config_defaults['batch_size'] = args.batch_size
-        self.save_freq = args.save_freq
-
-        config_defaults['init_lr'] = args.init_lr
-        config_defaults['d_lr'] = args.d_lr
-        config_defaults['g_lr'] = args.g_lr
-
-        """ Weight """
-        config_defaults['g_adv_weight'] = args.g_adv_weight
-        config_defaults['d_adv_weight'] = args.d_adv_weight
-        config_defaults['con_weight'] = args.con_weight
-        config_defaults['sty_weight'] = args.sty_weight
-        config_defaults['color_weight'] = args.color_weight
-        config_defaults['tv_weight'] = args.tv_weight
-
-        config_defaults['training_rate'] = args.training_rate
-        config_defaults['ld'] = args.ld
+        config_defaults = {'real_loss_weight': 1.7, 'fake_loss_weight': 1.7, 'gray_loss_weight': 1.7,
+                           'real_blur_loss_weight': 1.0, 'epoch': args.epoch, 'init_epoch': args.init_epoch,
+                           'gan_type': args.gan_type, 'batch_size': args.batch_size, 'init_lr': args.init_lr,
+                           'd_lr': args.d_lr, 'g_lr': args.g_lr, 'g_adv_weight': args.g_adv_weight,
+                           'd_adv_weight': args.d_adv_weight, 'con_weight': args.con_weight,
+                           'sty_weight': args.sty_weight, 'color_weight': args.color_weight,
+                           'tv_weight': args.tv_weight, 'training_rate': args.training_rate, 'ld': args.ld}
 
         # Initialize a new wandb run
         if self.hyperparameters:
@@ -53,8 +28,11 @@ class AnimeGANv2(object):
         else:
             wandb.init(config=config_defaults, project="AnimeGanV2", entity="roger_ds", sync_tensorboard=True)
 
-        # Config is a variable that holds and saves hyperparameters and inputs
-        self.config = wandb.config
+        self.model_name = 'AnimeGANv2'
+        self.checkpoint_dir = args.checkpoint_dir
+        self.log_dir = args.log_dir
+        self.dataset_name = args.dataset
+        self.save_freq = args.save_freq
 
         self.img_size = args.img_size
         self.img_ch = args.img_ch
@@ -67,29 +45,29 @@ class AnimeGANv2(object):
         self.sample_dir = os.path.join(args.sample_dir, self.model_dir)
         check_folder(self.sample_dir)
 
-        self.real_image_generator = ImageGenerator('./dataset/train_photo', self.img_size, self.config.batch_size)
+        self.real_image_generator = ImageGenerator('./dataset/train_photo', self.img_size, wandb.config.batch_size)
         self.anime_image_generator = ImageGenerator('./dataset/{}'.format(self.dataset_name + '/style'), self.img_size,
-                                                    self.config.batch_size)
+                                                    wandb.config.batch_size)
         self.anime_smooth_generator = ImageGenerator('./dataset/{}'.format(self.dataset_name + '/smooth'),
-                                                     self.img_size, self.config.batch_size)
+                                                     self.img_size, wandb.config.batch_size)
         self.dataset_num = max(self.real_image_generator.num_images, self.anime_image_generator.num_images)
 
         self.p_model = local_variables_init()
 
         print()
         print("##### Information #####")
-        print("# gan type : ", self.config.gan_type)
+        print("# gan type : ", wandb.config.gan_type)
         print("# dataset : ", self.dataset_name)
         print("# max dataset number : ", self.dataset_num)
-        print("# batch_size : ", self.config.batch_size)
-        print("# epoch : ", self.config.epoch)
-        print("# init_epoch : ", self.config.init_epoch)
+        print("# batch_size : ", wandb.config.batch_size)
+        print("# epoch : ", wandb.config.epoch)
+        print("# init_epoch : ", wandb.config.init_epoch)
         print("# training image size [H, W] : ", self.img_size)
-        print("# g_adv_weight,d_adv_weight,con_weight,sty_weight,color_weight,tv_weight : ", self.config.g_adv_weight,
-              self.config.d_adv_weight, self.config.con_weight, self.config.sty_weight, self.config.color_weight,
-              self.config.tv_weight)
-        print("# init_lr,g_lr,d_lr : ", self.config.init_lr, self.config.g_lr, self.config.d_lr)
-        print(f"# training_rate G -- D: {self.config.training_rate} : 1")
+        print("# g_adv_weight,d_adv_weight,con_weight,sty_weight,color_weight,tv_weight : ", wandb.config.g_adv_weight,
+              wandb.config.d_adv_weight, wandb.config.con_weight, wandb.config.sty_weight, wandb.config.color_weight,
+              wandb.config.tv_weight)
+        print("# init_lr,g_lr,d_lr : ", wandb.config.init_lr, wandb.config.g_lr, wandb.config.d_lr)
+        print(f"# training_rate G -- D: {wandb.config.training_rate} : 1")
         print()
 
     ##################################################################################
@@ -117,14 +95,14 @@ class AnimeGANv2(object):
     ##################################################################################
     @tf.function
     def gradient_panalty(self, real, fake):
-        if self.config.gan_type.__contains__('dragan'):
+        if wandb.config.gan_type.__contains__('dragan'):
             eps = tf.random.uniform(shape=tf.shape(real), minval=0., maxval=1.)
             _, x_var = tf.nn.moments(real, axes=[0, 1, 2, 3])
             x_std = tf.sqrt(x_var)  # magnitude of noise decides the size of local region
 
             fake = real + 0.5 * x_std * eps
 
-        alpha = tf.random.uniform(shape=[self.config.batch_size, 1, 1, 1], minval=0., maxval=1.)
+        alpha = tf.random.uniform(shape=[wandb.config.batch_size, 1, 1, 1], minval=0., maxval=1.)
         interpolated = real + alpha * (fake - real)
 
         disc = self.discriminator()
@@ -139,11 +117,11 @@ class AnimeGANv2(object):
 
         GP = 0
         # WGAN - LP
-        if self.config.gan_type.__contains__('lp'):
-            GP = self.config.ld * tf.reduce_mean(tf.square(tf.maximum(0.0, grad_norm - 1.)))
+        if wandb.config.gan_type.__contains__('lp'):
+            GP = wandb.config.ld * tf.reduce_mean(tf.square(tf.maximum(0.0, grad_norm - 1.)))
 
-        elif self.config.gan_type.__contains__('gp') or self.config.gan_type == 'dragan':
-            GP = self.config.ld * tf.reduce_mean(tf.square(grad_norm - 1.))
+        elif wandb.config.gan_type.__contains__('gp') or wandb.config.gan_type == 'dragan':
+            GP = wandb.config.ld * tf.reduce_mean(tf.square(grad_norm - 1.))
 
         return GP
 
@@ -165,9 +143,9 @@ class AnimeGANv2(object):
 
         """ Training """
 
-        init_optim = Adam(self.config.init_lr, beta_1=0.5, beta_2=0.999)
-        G_optim = Adam(self.config.g_lr, beta_1=0.5, beta_2=0.999)
-        D_optim = Adam(self.config.d_lr, beta_1=0.5, beta_2=0.999)
+        init_optim = Adam(wandb.config.init_lr, beta_1=0.5, beta_2=0.999)
+        G_optim = Adam(wandb.config.g_lr, beta_1=0.5, beta_2=0.999)
+        D_optim = Adam(wandb.config.d_lr, beta_1=0.5, beta_2=0.999)
 
         # saver to save model
         self.saver = tf.train.Checkpoint(generated=generated, discriminator=discriminator, G_optim=G_optim,
@@ -186,9 +164,9 @@ class AnimeGANv2(object):
 
         init_mean_loss = []
         mean_loss = []
-        j = self.config.training_rate
-        for epoch in range(start_epoch, self.config.epoch):
-            total_step = int(self.dataset_num / self.config.batch_size)
+        j = wandb.config.training_rate
+        for epoch in range(start_epoch, wandb.config.epoch):
+            total_step = int(self.dataset_num / wandb.config.batch_size)
             with tqdm(range(total_step)) as tbar:
                 for step in range(total_step):
                     real = next(real_img_op)[0]
@@ -196,7 +174,7 @@ class AnimeGANv2(object):
                     anime_gray = next(anime_img_op)[1]
                     anime_smooth = next(anime_smooth_op)[0]
 
-                    if epoch < self.config.init_epoch:
+                    if epoch < wandb.config.init_epoch:
                         init_loss = self.init_train_step(generated, init_optim, epoch, real)
                         init_mean_loss.append(init_loss)
                         tbar.set_description('Epoch %d' % epoch)
@@ -205,7 +183,7 @@ class AnimeGANv2(object):
                         if (step + 1) % 200 == 0:
                             init_mean_loss.clear()
                     else:
-                        if j == self.config.training_rate:
+                        if j == wandb.config.training_rate:
                             # Update D network
                             d_loss = self.d_train_step(real, anime, anime_gray, anime_smooth,
                                                        generated, discriminator, D_optim, epoch)
@@ -215,7 +193,7 @@ class AnimeGANv2(object):
 
                         mean_loss.append([d_loss, g_loss])
                         tbar.set_description('Epoch %d' % epoch)
-                        if j == self.config.training_rate:
+                        if j == wandb.config.training_rate:
                             tbar.set_postfix(d_loss=d_loss.numpy(), g_loss=g_loss.numpy(),
                                              mean_d_loss=np.mean(mean_loss, axis=0)[0],
                                              mean_g_loss=np.mean(mean_loss, axis=0)[1])
@@ -228,18 +206,17 @@ class AnimeGANv2(object):
 
                         j = j - 1
                         if j < 1:
-                            j = self.config.training_rate
+                            j = wandb.config.training_rate
             if not self.hyperparameters:
-                if (epoch + 1) >= self.config.init_epoch and np.mod(epoch + 1, self.save_freq) == 0:
+                if (epoch + 1) >= wandb.config.init_epoch and np.mod(epoch + 1, self.save_freq) == 0:
                     self.save(self.checkpoint_dir, epoch)
 
-            if epoch >= self.config.init_epoch - 1:
+            if epoch >= wandb.config.init_epoch - 1:
                 """ Result Image """
                 val_files = glob('./dataset/{}/*.*'.format('val'))
                 # save_path = './{}/{:03d}/'.format(self.sample_dir, epoch)
                 # check_folder(save_path)
                 val_images = []
-                val_images_tensorboard = []
                 for i, sample_file in enumerate(val_files):
                     print('val: ' + str(i) + sample_file)
                     sample_image = np.asarray(load_test_data(sample_file, self.img_size))
@@ -248,12 +225,11 @@ class AnimeGANv2(object):
                     # save_images(test_real, save_path + '{:03d}_a.jpg'.format(i), None)
                     # save_images(test_generated_predict, save_path + '{:03d}_b.jpg'.format(i), None)
                     val_images.append(wandb.Image(test_generated_predict, caption="Name:{}, epoch:{}".format(i, epoch)))
-                    val_images_tensorboard.append(test_generated_predict)
-                with self.writer.as_default(step=epoch):
-                    """" Summary """
-                    images = np.reshape(val_images_tensorboard, (-1, 64, 64, 3))
-                    tf.summary.image(name='val_data', data=images, max_outputs=30, step=epoch)
-                wandb.log('val_data', val_images, step=epoch)
+                    if i == 0 or i == 26 or i == 5:
+                        with self.writer.as_default(step=epoch):
+                            """" Summary """
+                            tf.summary.image(name='val_data_' + str(i), data=test_generated_predict, step=epoch)
+                wandb.log({'val_data': val_images}, step=epoch)
                 if not self.hyperparameters:
                     save_model_path = 'save_model'
                     if not os.path.exists(save_model_path):
@@ -266,7 +242,7 @@ class AnimeGANv2(object):
             generator_images = generated(real)
             # init pharse
             init_c_loss = con_loss(self.p_model, real, generator_images)
-            init_loss = self.config.con_weight * init_c_loss
+            init_loss = wandb.config.con_weight * init_c_loss
         grads = tape.gradient(init_loss, generated.trainable_variables)
         init_optim.apply_gradients(zip(grads, generated.trainable_variables))
         # wandb.log("G_init", init_loss.numpy(), step=epoch)
@@ -284,11 +260,11 @@ class AnimeGANv2(object):
             generated_logit = discriminator(fake_image)
             # gan
             c_loss, s_loss = con_sty_loss(self.p_model, real, anime_gray, fake_image)
-            tv_loss = self.config.tv_weight * total_variation_loss(fake_image)
+            tv_loss = wandb.config.tv_weight * total_variation_loss(fake_image)
             col_loss = color_loss(real, fake_image)
-            t_loss = self.config.con_weight * c_loss + self.config.sty_weight * s_loss + col_loss * self.config.color_weight + tv_loss
+            t_loss = wandb.config.con_weight * c_loss + wandb.config.sty_weight * s_loss + col_loss * wandb.config.color_weight + tv_loss
 
-            g_loss = self.config.g_adv_weight * generator_loss(self.config.gan_type, generated_logit)
+            g_loss = wandb.config.g_adv_weight * generator_loss(wandb.config.gan_type, generated_logit)
             Generator_loss = t_loss + g_loss
 
         grads = tape.gradient(Generator_loss, generated.trainable_variables)
@@ -322,20 +298,20 @@ class AnimeGANv2(object):
             d_smooth_logit = discriminator(anime_smooth)
             generated_logit = discriminator(fake_image)
             """ Define Loss """
-            if self.config.gan_type.__contains__('gp') or self.config.gan_type.__contains__('lp') or \
-                    self.config.gan_type.__contains__('dragan'):
+            if wandb.config.gan_type.__contains__('gp') or wandb.config.gan_type.__contains__('lp') or \
+                    wandb.config.gan_type.__contains__('dragan'):
                 GP = self.gradient_panalty(real=anime, fake=fake_image)
             else:
                 GP = 0.0
 
-            d_loss = self.config.d_adv_weight * discriminator_loss(self.config.gan_type, d_anime_logit,
+            d_loss = wandb.config.d_adv_weight * discriminator_loss(wandb.config.gan_type, d_anime_logit,
                                                                    d_anime_gray_logit,
                                                                    generated_logit,
                                                                    d_smooth_logit, epoch, self.writer,
-                                                                   self.config.real_loss_weight,
-                                                                   self.config.fake_loss_weight,
-                                                                   self.config.gray_loss_weight,
-                                                                   self.config.real_blur_loss_weight) + GP
+                                                                   wandb.config.real_loss_weight,
+                                                                   wandb.config.fake_loss_weight,
+                                                                   wandb.config.gray_loss_weight,
+                                                                   wandb.config.real_blur_loss_weight) + GP
         grads = tape.gradient(d_loss, discriminator.trainable_variables)
         D_optim.apply_gradients(zip(grads, discriminator.trainable_variables))
 
@@ -348,10 +324,10 @@ class AnimeGANv2(object):
     @property
     def model_dir(self):
         return "{}_{}_{}_{}_{}_{}_{}_{}_{}".format(self.model_name, self.dataset_name,
-                                                   self.config.gan_type,
-                                                   int(self.config.g_adv_weight), int(self.config.d_adv_weight),
-                                                   int(self.config.con_weight), int(self.config.sty_weight),
-                                                   int(self.config.color_weight), int(self.config.tv_weight))
+                                                   wandb.config.gan_type,
+                                                   int(wandb.config.g_adv_weight), int(wandb.config.d_adv_weight),
+                                                   int(wandb.config.con_weight), int(wandb.config.sty_weight),
+                                                   int(wandb.config.color_weight), int(wandb.config.tv_weight))
 
     def save(self, checkpoint_dir, epoch):
         checkpoint_dir = os.path.join(checkpoint_dir, self.model_dir)
