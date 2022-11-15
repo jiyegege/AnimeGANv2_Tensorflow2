b10ac748899ec1819a68fb7636f39faa9cccb77d
roger <484938670@qq.com>
2022-11-15 11:23:12 +0800

    fix(v1.0): fix some bug

--- net/backtone.py
+++ net/backtone.py
@@ -101,15 +101,17 @@ class VGG19Conv4(Model):
         conv1_1_biases = self.get_bias('conv1_1')
         self.conv1_1 = Conv2D(conv1_1_filter_shape[3], kernel_size=(conv1_1_filter_shape[0], conv1_1_filter_shape[1]),
                               padding='same', activation='relu',
-                              strides=(1, 1), name='conv1_1', use_bias=True, bias_constraint=conv1_1_biases,
-                              kernel_constraint=conv1_1_filter)
+                              strides=(1, 1), name='conv1_1', use_bias=True,
+                              bias_initializer=tf.keras.initializers.Constant(conv1_1_biases),
+                              kernel_initializer=tf.keras.initializers.Constant(conv1_1_filter))
         conv1_2_filter = self.get_conv_filter('conv1_2')
         conv1_2_filter_shape = conv1_2_filter.shape
         conv1_2_biases = self.get_bias('conv1_2')
         self.conv1_2 = Conv2D(conv1_2_filter_shape[3], kernel_size=(conv1_2_filter_shape[0], conv1_2_filter_shape[1]),
                               padding='same', activation='relu',
-                              strides=(1, 1), name='conv1_2', use_bias=True, bias_constraint=conv1_2_biases,
-                              kernel_constraint=conv1_2_filter)
+                              strides=(1, 1), name='conv1_2', use_bias=True,
+                              bias_initializer=tf.keras.initializers.Constant(conv1_2_biases),
+                              kernel_initializer=tf.keras.initializers.Constant(conv1_2_filter))
         self.pool1 = MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='same')
 
         conv2_1_filter = self.get_conv_filter('conv2_1')
@@ -117,15 +119,17 @@ class VGG19Conv4(Model):
         conv2_1_biases = self.get_bias('conv2_1')
         self.conv2_1 = Conv2D(conv2_1_filter_shape[3], kernel_size=(conv2_1_filter_shape[0], conv2_1_filter_shape[1]),
                               padding='same', activation='relu',
-                              strides=(1, 1), name='conv2_1', use_bias=True, bias_constraint=conv2_1_biases,
-                              kernel_constraint=conv2_1_filter)
+                              strides=(1, 1), name='conv2_1', use_bias=True,
+                              bias_initializer=tf.keras.initializers.Constant(conv2_1_biases),
+                              kernel_initializer=tf.keras.initializers.Constant(conv2_1_filter))
         conv2_2_filter = self.get_conv_filter('conv2_2')
         conv2_2_filter_shape = conv2_2_filter.shape
         conv2_2_biases = self.get_bias('conv2_2')
         self.conv2_2 = Conv2D(conv2_2_filter_shape[3], kernel_size=(conv2_2_filter_shape[0], conv2_2_filter_shape[1]),
                               padding='same', activation='relu',
-                              strides=(1, 1), name='conv2_2', use_bias=True, bias_constraint=conv2_2_biases,
-                              kernel_constraint=conv2_2_filter)
+                              strides=(1, 1), name='conv2_2', use_bias=True,
+                              bias_initializer=tf.keras.initializers.Constant(conv2_2_biases),
+                              kernel_initializer=tf.keras.initializers.Constant(conv2_2_filter))
         self.pool2 = MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='same')
 
         conv3_1_filter = self.get_conv_filter('conv3_1')
@@ -133,29 +137,33 @@ class VGG19Conv4(Model):
         conv3_1_biases = self.get_bias('conv3_1')
         self.conv3_1 = Conv2D(conv3_1_filter_shape[3], kernel_size=(conv3_1_filter_shape[0], conv3_1_filter_shape[1]),
                               padding='same', activation='relu',
-                              strides=(1, 1), name='conv3_1', use_bias=True, bias_constraint=conv3_1_biases,
-                              kernel_constraint=conv3_1_filter)
+                              strides=(1, 1), name='conv3_1', use_bias=True,
+                              bias_initializer=tf.keras.initializers.Constant(conv3_1_biases),
+                              kernel_initializer=tf.keras.initializers.Constant(conv3_1_filter))
         conv3_2_filter = self.get_conv_filter('conv3_2')
         conv3_2_filter_shape = conv3_2_filter.shape
         conv3_2_biases = self.get_bias('conv3_2')
         self.conv3_2 = Conv2D(conv3_2_filter_shape[3], kernel_size=(conv3_2_filter_shape[0], conv3_2_filter_shape[1]),
                               padding='same', activation='relu',
-                              strides=(1, 1), name='conv3_2', use_bias=True, bias_constraint=conv3_2_biases,
-                              kernel_constraint=conv3_2_filter)
+                              strides=(1, 1), name='conv3_2', use_bias=True,
+                              bias_initializer=tf.keras.initializers.Constant(conv3_2_biases),
+                              kernel_initializer=tf.keras.initializers.Constant(conv3_2_filter))
         conv3_3_filter = self.get_conv_filter('conv3_3')
         conv3_3_filter_shape = conv3_3_filter.shape
         conv3_3_biases = self.get_bias('conv3_3')
         self.conv3_3 = Conv2D(conv3_3_filter_shape[3], kernel_size=(conv3_3_filter_shape[0], conv3_3_filter_shape[1]),
                               padding='same', activation='relu',
-                              strides=(1, 1), name='conv3_3', use_bias=True, bias_constraint=conv3_3_biases,
-                              kernel_constraint=conv3_3_filter)
+                              strides=(1, 1), name='conv3_3', use_bias=True,
+                              bias_initializer=tf.keras.initializers.Constant(conv3_3_biases),
+                              kernel_initializer=tf.keras.initializers.Constant(conv3_3_filter))
         conv3_4_filter = self.get_conv_filter('conv3_4')
         conv3_4_filter_shape = conv3_4_filter.shape
         conv3_4_biases = self.get_bias('conv3_4')
         self.conv3_4 = Conv2D(conv3_4_filter_shape[3], kernel_size=(conv3_4_filter_shape[0], conv3_4_filter_shape[1]),
                               padding='same', activation='relu',
-                              strides=(1, 1), name='conv3_4', use_bias=True, bias_constraint=conv3_4_biases,
-                              kernel_constraint=conv3_4_filter)
+                              strides=(1, 1), name='conv3_4', use_bias=True,
+                              bias_initializer=tf.keras.initializers.Constant(conv3_4_biases),
+                              kernel_initializer=tf.keras.initializers.Constant(conv3_4_filter))
         self.pool3 = MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='same')
 
         conv4_1_filter = self.get_conv_filter('conv4_1')
@@ -163,22 +171,25 @@ class VGG19Conv4(Model):
         conv4_1_biases = self.get_bias('conv4_1')
         self.conv4_1 = Conv2D(conv4_1_filter_shape[3], kernel_size=(conv4_1_filter_shape[0], conv4_1_filter_shape[1]),
                               padding='same', activation='relu',
-                              strides=(1, 1), name='conv4_1', use_bias=True, bias_constraint=conv4_1_biases,
-                              kernel_constraint=conv4_1_filter)
+                              strides=(1, 1), name='conv4_1', use_bias=True,
+                              bias_initializer=tf.keras.initializers.Constant(conv4_1_biases),
+                              kernel_initializer=tf.keras.initializers.Constant(conv4_1_filter))
         conv4_2_filter = self.get_conv_filter('conv4_2')
         conv4_2_filter_shape = conv4_2_filter.shape
         conv4_2_biases = self.get_bias('conv4_2')
         self.conv4_2 = Conv2D(conv4_2_filter_shape[3], kernel_size=(conv4_2_filter_shape[0], conv4_2_filter_shape[1]),
                               padding='same', activation='relu',
-                              strides=(1, 1), name='conv4_2', use_bias=True, bias_constraint=conv4_2_biases,
-                              kernel_constraint=conv4_2_filter)
+                              strides=(1, 1), name='conv4_2', use_bias=True,
+                              bias_initializer=tf.keras.initializers.Constant(conv4_2_biases),
+                              kernel_initializer=tf.keras.initializers.Constant(conv4_2_filter))
         conv4_3_filter = self.get_conv_filter('conv4_3')
         conv4_3_filter_shape = conv4_3_filter.shape
         conv4_3_biases = self.get_bias('conv4_3')
         self.conv4_3 = Conv2D(conv4_3_filter_shape[3], kernel_size=(conv4_3_filter_shape[0], conv4_3_filter_shape[1]),
                               padding='same', activation='relu',
-                              strides=(1, 1), name='conv4_3', use_bias=True, bias_constraint=conv4_3_biases,
-                              kernel_constraint=conv4_3_filter)
+                              strides=(1, 1), name='conv4_3', use_bias=True,
+                              bias_initializer=tf.keras.initializers.Constant(conv4_3_biases),
+                              kernel_initializer=tf.keras.initializers.Constant(conv4_3_filter))
 
         conv4_4_filter = self.get_conv_filter('conv4_4')
         conv4_4_filter_shape = conv4_4_filter.shape
@@ -187,8 +198,8 @@ class VGG19Conv4(Model):
                                             kernel_size=(conv4_4_filter_shape[0], conv4_4_filter_shape[1]),
                                             padding='same', activation=None,
                                             strides=(1, 1), name='conv4_4', use_bias=True,
-                                            bias_constraint=conv4_4_biases,
-                                            kernel_constraint=conv4_4_filter)
+                                            bias_initializer=tf.keras.initializers.Constant(conv4_4_biases),
+                                            kernel_initializer=tf.keras.initializers.Constant(conv4_4_filter))
 
     def call(self, inputs, training=False, mask=None):
         rgb_scaled = ((inputs + 1) / 2) * 255.0  # [-1, 1] ~ [0, 255]
